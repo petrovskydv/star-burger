0d551d1d689a9fdeeb1aa75a4e919acadfebc5fe
Денис Петровский <petrovsky.dv@gmail.com>
2021-05-29 13:34:05 +0300

    add location app

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -1,7 +1,6 @@
 import os
 
 import dj_database_url
-
 from environs import Env
 
 env = Env()
@@ -17,6 +16,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
     'restaurateur.apps.RestaurateurConfig',
+    'location.apps.LocationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
