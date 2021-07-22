3d7af6e8bd0661300551755d8676cc92eca4f82f
Денис Петровский <petrovsky.dv@gmail.com>
2021-07-22 16:38:35 +0300

    fix field of location model

--- location/models.py
+++ location/models.py
@@ -5,7 +5,7 @@ class Place(models.Model):
     address = models.CharField('адрес', max_length=100, unique=True)
     latitude = models.FloatField('широта', blank=True, null=True)
     longitude = models.FloatField('долгота', blank=True, null=True)
-    received_at = models.DateTimeField('дата получения координат', auto_now=True, null=True)
+    received_at = models.DateTimeField('дата получения координат', auto_now=True)
 
     class Meta:
         verbose_name = 'место'
