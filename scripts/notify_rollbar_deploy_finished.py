4d37043a7ac5c1f381e0e41de8829c2cd69249ca
syncmas <petrovsky.dv@gmail.com>
2021-11-18 16:30:21 +0300

    fix notify_rollbar_deploy_finished.py
    /spend 38 minutes

--- scripts/notify_rollbar_deploy_finished.py
+++ scripts/notify_rollbar_deploy_finished.py
@@ -1,16 +1,20 @@
-import requests
 import subprocess
-from django.conf import settings
 
+import requests
+from environs import Env
+
+env = Env()
+env.read_env()
 # fetch last committed revision in the locally-checked out branch
 git_process = subprocess.run(['git', 'log', '-n', '1', '--pretty=format:%H'], capture_output=True, check=True)
 revision = git_process.stdout.decode()
 
 response = requests.post('https://api.rollbar.com/api/1/deploy/', json={
-    'environment': settings.ROLLBAR['environment'],
+    'environment': env('ROLLBAR_ENVIRONMENT', 'development'),
     'revision': revision
 }, headers={
-    'X-Rollbar-Access-Token': settings.ROLLBAR['access_token'],
+    'X-Rollbar-Access-Token': env('ROLLBAR_TOKEN'),
 }, timeout=3)
 
 response.raise_for_status()
+print(response.json())
